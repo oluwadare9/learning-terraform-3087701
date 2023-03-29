fd7d8ec30b34c682e9698b53b7fd179a21da0b70
oluwadare9 <129242954+oluwadare9@users.noreply.github.com>
2023-03-29 13:18:54 -0400

    security group changes

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_security_group_rule" "web_http_in" {
   from_port = 80
   to_port = 80
   protocol = "tcp"
-  cidr_blocks = {"0.0.0.0/0"}
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.web.id
 } 
@@ -52,7 +52,7 @@ resource "aws_security_group_rule" "web_https_in" {
   from_port = 443
   to_port = 443
   protocol = "tcp"
-  cidr_blocks = {"0.0.0.0/0"}
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.web.id
 } 
@@ -62,7 +62,7 @@ resource "aws_security_group_rule" "web_everything_out" {
   from_port = 0
   to_port = 0
   protocol = "-1"
-  cidr_blocks = {"0.0.0.0/0"}
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.web.id
 } 
